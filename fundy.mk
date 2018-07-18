a520d7bd1ce1e2873c388192d26fa0ba22e42188
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-07-18 05:03:50 -0700

    fundy: update pak binary hook up support.
    refs #SWRAA-304
    
    Change-Id: I468026243d5f65f0d130de274e5f896ffc9bd112
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- fundy.mk
+++ fundy.mk
@@ -105,7 +105,7 @@ export BOLT_IMG_SWAP_BFW            := device/broadcom/fundy/blb/zb/bfw-2.1.3-zb
 export BOLT_IMG_SWAP_RD             := device/broadcom/fundy/blb/zb/rd-zb.bin
 
 # TODO: fix up the zd|zb use case.
-export LOCAL_DEVICE_PAK_BINARY   := pak.7278.zd.bin
+export LOCAL_DEVICE_PAK_BINARY_DEV  := pak.7278.zd.bin
 
 # get sage bin's from 7278B0.
 export SAGE_BL_BINARY_PATH       := vendor/broadcom/sage/7278B0/dev
