ccc6249173ca64d9a2b8efe76fd4d8b6b3fb8bed
Edwin Chan <edwin.chan@broadcom.com>
2019-07-10 15:04:19 -0700

    fundy_ms12d: Use default EOTF mode
    refs #SWANDROID-6732
    
    Change-Id: Ib71974e5d4daa92677c43f2c2a3414857dfd578e
    Signed-off-by: Edwin Chan <edwin.chan@broadcom.com>
    (cherry picked from commit 6692250cd2714139d41a14a891edda8cc607bd17)

--- fundy_ms12d.mk
+++ fundy_ms12d.mk
@@ -3,6 +3,9 @@ export LOCAL_CFG_PROFILE       := ms12d
 export BDSP_MS12_SUPPORT       := D
 include device/broadcom/fundy/fundy.mk
 
+# Disable force_eotf tweak
+PRODUCT_PROPERTY_OVERRIDES := $(subst ro.nx.hwc2.tweak.force_eotf=0,,$(PRODUCT_PROPERTY_OVERRIDES))
+
 PRODUCT_NAME                   := fundy_ms12d
 PRODUCT_MODEL                  := fundy_ms12d
 PRODUCT_BRAND                  := google
